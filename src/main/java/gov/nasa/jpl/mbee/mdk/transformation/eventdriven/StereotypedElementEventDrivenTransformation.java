3d51b833f12b7cc1a7f254b20f90c412d8f3774f
lunkpeter <lunk.peter@incquerylabs.com>
2017-09-22 14:30:26 +0200

    #3 Final modifications, added javadoc to transformation classes.

--- src/main/java/gov/nasa/jpl/mbee/mdk/transformation/eventdriven/StereotypedElementEventDrivenTransformation.java
+++ src/main/java/gov/nasa/jpl/mbee/mdk/transformation/eventdriven/StereotypedElementEventDrivenTransformation.java
@@ -40,60 +40,60 @@ import gov.nasa.jpl.mbee.mdk.queries.util.TransformedStereotypedBlocksQuerySpeci
 import gov.nasa.jpl.mbee.mdk.transformation.util.EngineCreationUtil;
 import gov.nasa.jpl.mbee.mdk.transformation.util.StereotypedElementTransformationActions;
 
-public class StereotypedElementEventDrivenTransformation extends AdapterImpl{	
-	
+public class StereotypedElementEventDrivenTransformation extends AdapterImpl {
+
 	private AdvancedViatraQueryEngine engine;
 	private EventDrivenTransformation transformation;
 	private EventDrivenTransformationRuleFactory ruleFactory = new EventDrivenTransformationRuleFactory();
 
 	private Collection<Stereotype> stereotypes;
-	
-	//Transformation rule responsible for creating attributes in elements in accordance to the stereotype tagged values
+
+	// Transformation rule responsible for creating attributes in elements in
+	// accordance to the stereotype tagged values
 	private EventDrivenTransformationRule<StereotypedBlocksMatch, StereotypedBlocksMatcher> attributeCreation;
-	//Transformation rule responsible for setting redefinition relations between parent and child attributes
+	// Transformation rule responsible for setting redefinition relations between
+	// parent and child attributes
 	private EventDrivenTransformationRule<TransformedGeneralizedBlockPairsMatch, TransformedGeneralizedBlockPairsMatcher> attributeRedefinition;
-	//Rule responsible for removing stereotype instances from classes
+	// Rule responsible for removing stereotype instances from classes
 	private EventDrivenTransformationRule<TransformedStereotypedBlocksMatch, TransformedStereotypedBlocksMatcher> stereotypeRemoval;
-	
+	// Rule responsible for creating implied parent classes
 	private EventDrivenTransformationRule<FindCommonParentClassMatch, FindCommonParentClassMatcher> blockRestructuring;
 
-	
-	public StereotypedElementEventDrivenTransformation(Collection<Stereotype> stereotypes) throws ViatraQueryException, InconsistentEventSemanticsException {
-		
+	public StereotypedElementEventDrivenTransformation(Collection<Stereotype> stereotypes)
+			throws ViatraQueryException, InconsistentEventSemanticsException {
+
 		this.engine = EngineCreationUtil.createEngine();
 		this.stereotypes = stereotypes;
 		initTransformation();
 	}
-	
+
 	private void initTransformation() throws ViatraQueryException, InconsistentEventSemanticsException {
-		//Create VIATRA Batch transformation
-		
+		// Create VIATRA Batch transformation
+
 		EventDrivenTransformationQueries.instance().prepare(engine);
-		
+
 		transformation = EventDrivenTransformation.forEngine(engine)
-//				.addAdapterConfiguration(new TransformationDebuggerConfiguration("MDK_TRAFO"))
-				.addRule(getAttributeCreationRule())
-				.addRule(getAttributeRedefinitionRule())
-				.addRule(getStereotypeRemovalRule())
-				.addRule(getBlockRestructuringRule())
+				.addRule(getAttributeCreationRule()).addRule(getAttributeRedefinitionRule())
+				.addRule(getStereotypeRemovalRule()).addRule(getBlockRestructuringRule())
 				.setConflictResolver(createConflictResolver())
-				.setSchedulerFactory(getQueryEngineSchedulerFactory(engine))
-				.build();
+				.setSchedulerFactory(getQueryEngineSchedulerFactory(engine)).build();
 		transformation.getExecutionSchema().getLogger().setLevel(Level.DEBUG);
 	}
-	
+
 	public void forceExecution() throws ViatraQueryException, InconsistentEventSemanticsException {
 		transformation.getExecutionSchema().startUnscheduledExecution();
 	}
-	
+
 	/**
-	 * The returned rule creates attributes (value properties) based on the tag values of blocks labeled with the selected stereotypes (sterotypes list).
+	 * The returned rule creates attributes (value properties) based on the tag
+	 * values of blocks labeled with the selected stereotypes (stereotypes list).
 	 * This rule should be fired first as it does not depend on any other rule.
 	 * 
 	 * Precondition:
 	 * 
-	 * Returns blocks with tag values (a slot referring to a property). These properties are owned attributes of a stereotype.
-	 * A slot assigns a value to the property.
+	 * Returns blocks with tag values (a slot referring to a property). These
+	 * properties are owned attributes of a stereotype. A slot assigns a value to
+	 * the property.
 	 * 
 	 */
 	private EventDrivenTransformationRule<StereotypedBlocksMatch, StereotypedBlocksMatcher> getAttributeCreationRule()
@@ -103,32 +103,32 @@ public class StereotypedElementEventDrivenTransformation extends AdapterImpl{
 					.name("AttributesFromTagsRule").precondition(StereotypedBlocksQuerySpecification.instance())
 					.action(new IMatchProcessor<StereotypedBlocksMatch>() {
 						public void process(StereotypedBlocksMatch match) {
-							//TODO Workaround for VIATRA ED Transformation filtering bug
-							if(stereotypes.contains(match.getStereotype())) {
-								openSession("Creating block attributes");
-								StereotypedElementTransformationActions.createBlockAttributes(match.getProperty(), match.getBlock(), match.getValue());
-								closeSession();
-							}
-							
+							openSession("Creating block attributes");
+							StereotypedElementTransformationActions.createBlockAttributes(match.getProperty(),
+									match.getBlock(), match.getValue());
+							closeSession();
 						}
 
-						
 					}).filter(match -> {
 						System.out.println(match.getStereotype() + " : " + stereotypes.contains(match.getStereotype()));
-						return stereotypes.contains(match.getStereotype());}).build();
+						return stereotypes.contains(match.getStereotype());
+					}).build();
 		}
 		return attributeCreation;
 	}
-	
+
 	/**
-	 * The returned rule redefines attributes that are created by attributeCreation. The attribute of a block redefines the same attribute (same name and type) of the parent block (generalization between the blocks).
-	 * This rule should be fired after the attributeCreationRule.
+	 * The returned rule redefines attributes that are created by attributeCreation.
+	 * The attribute of a block redefines the same attribute (same name and type) of
+	 * the parent block (generalization between the blocks). This rule should be
+	 * fired after the attributeCreationRule.
 	 * 
 	 * Precondition:
 	 * 
-	 * Returns a pair of blocks that are in a generalization relationship (parent and child).
-	 * Also, it returns the stereotype that is assigned to the blocks, as well as the attributes
-	 * (parentAttribute and childAttribute) that should be in a redefinition relationship according to the desired design pattern.
+	 * Returns a pair of blocks that are in a generalization relationship (parent
+	 * and child). Also, it returns the stereotype that is assigned to the blocks,
+	 * as well as the attributes (parentAttribute and childAttribute) that should be
+	 * in a redefinition relationship according to the desired design pattern.
 	 * 
 	 */
 	private EventDrivenTransformationRule<TransformedGeneralizedBlockPairsMatch, TransformedGeneralizedBlockPairsMatcher> getAttributeRedefinitionRule()
@@ -140,44 +140,53 @@ public class StereotypedElementEventDrivenTransformation extends AdapterImpl{
 					.precondition(TransformedGeneralizedBlockPairsQuerySpecification.instance())
 					.action(new IMatchProcessor<TransformedGeneralizedBlockPairsMatch>() {
 						public void process(TransformedGeneralizedBlockPairsMatch match) {
-							//TODO Workaround for VIATRA ED Transformation filtering bug
-							if(stereotypes.contains(match.getStereotype())) {
-								
-								openSession("Setting redefinition relation");
-								StereotypedElementTransformationActions.createAttributeredefinition(match.getChildAttribute(), match.getParentAttribute());
-								closeSession();
-							}
-							
+							openSession("Setting redefinition relation");
+							StereotypedElementTransformationActions
+									.createAttributeredefinition(match.getChildAttribute(), match.getParentAttribute());
+							closeSession();
 						}
 					}).filter(match -> stereotypes.contains(match.getStereotype())).build();
 		}
 		return attributeRedefinition;
 	}
-	
+
 	/**
-	 * The returned rule removes the labels of the selected stereotypes (sterotypes list) and the inherent tagged values from blocks.
-	 * This rule should be fired after the attributeRedefinitionRule.
-	 *  
+	 * Rule responsible for removing now obsolete stereotype instances from the
+	 * related blocks. This rule should be fired after the
+	 * attributeRedefinitionRule.
+	 * 
 	 */
 	private EventDrivenTransformationRule<TransformedStereotypedBlocksMatch, TransformedStereotypedBlocksMatcher> getStereotypeRemovalRule()
 			throws InconsistentEventSemanticsException, ViatraQueryException {
 		if (stereotypeRemoval == null) {
-			stereotypeRemoval = ruleFactory.<TransformedStereotypedBlocksMatch, TransformedStereotypedBlocksMatcher>createRule()
-					.name("StereotypeRemovalRule").precondition(TransformedStereotypedBlocksQuerySpecification.instance())
+			stereotypeRemoval = ruleFactory
+					.<TransformedStereotypedBlocksMatch, TransformedStereotypedBlocksMatcher>createRule()
+					.name("StereotypeRemovalRule")
+					.precondition(TransformedStereotypedBlocksQuerySpecification.instance())
 					.action(new IMatchProcessor<TransformedStereotypedBlocksMatch>() {
 						public void process(TransformedStereotypedBlocksMatch match) {
-							//TODO Workaround for VIATRA ED Transformation filtering bug
-							if(stereotypes.contains(match.getStereotype())) {
-								openSession("Removing stereotype instance");
-								StereotypedElementTransformationActions.createRemoveStereotypeInstance(match.getSlot(), match.getStereotype());
-								closeSession();
-							}
+							openSession("Removing stereotype instance");
+							StereotypedElementTransformationActions.createRemoveStereotypeInstance(match.getSlot(),
+									match.getStereotype());
+							closeSession();
 						}
 					}).filter(match -> stereotypes.contains(match.getStereotype())).build();
 		}
 		return stereotypeRemoval;
 	}
-	
+
+	/**
+	 * Rule responsible for creating implied parent classes. Parent classes are
+	 * implied when two blocks introduce the same property and define a deafult
+	 * value for it. In this case a new common base block is created that defines
+	 * said property. Generalizations are added from the original blocks to the new
+	 * parent block, and their properties redefine the parent's new properties as
+	 * well.
+	 * 
+	 * @return
+	 * @throws InconsistentEventSemanticsException
+	 * @throws ViatraQueryException
+	 */
 	private EventDrivenTransformationRule<FindCommonParentClassMatch, FindCommonParentClassMatcher> getBlockRestructuringRule()
 			throws InconsistentEventSemanticsException, ViatraQueryException {
 		if (blockRestructuring == null) {
@@ -185,34 +194,38 @@ public class StereotypedElementEventDrivenTransformation extends AdapterImpl{
 					.name("BlockRestructuringRule").precondition(FindCommonParentClassQuerySpecification.instance())
 					.action(new IMatchProcessor<FindCommonParentClassMatch>() {
 						public void process(FindCommonParentClassMatch match) {
-							//TODO Workaround for VIATRA ED Transformation filtering bug
-							if(stereotypes.contains(match.getStereotype())) {
-								openSession("Restructuring block hierarchy");
-								try {
-									StereotypedElementTransformationActions.createImpliedParentBlock(match);
-								} catch (ReadOnlyElementException e) {
-									e.printStackTrace();
-								}
-								closeSession();
+							openSession("Restructuring block hierarchy");
+							try {
+								StereotypedElementTransformationActions.createImpliedParentBlock(match);
+							} catch (ReadOnlyElementException e) {
+								e.printStackTrace();
 							}
+							closeSession();
 						}
 					}).filter(match -> stereotypes.contains(match.getStereotype())).build();
 		}
 		return blockRestructuring;
 	}
-	
+
+	/*
+	 * Utility methods for creating and closing MD sessions on demand
+	 */
 	private void openSession(String message) {
-		if(!SessionManager.getInstance().isSessionCreated(Application.getInstance().getProject())) {
-			SessionManager.getInstance().createSession(Application.getInstance().getProject(), message+System.nanoTime());
+		if (!SessionManager.getInstance().isSessionCreated(Application.getInstance().getProject())) {
+			SessionManager.getInstance().createSession(Application.getInstance().getProject(),
+					message + System.nanoTime());
 		}
 	}
-	
+
 	private void closeSession() {
-		if(SessionManager.getInstance().isSessionCreated(Application.getInstance().getProject())) {
+		if (SessionManager.getInstance().isSessionCreated(Application.getInstance().getProject())) {
 			SessionManager.getInstance().closeSession(Application.getInstance().getProject());
 		}
 	}
-		
+
+	/*
+	 * On dispose destroy the created engine and transformation
+	 */
 	public void dispose() {
 		if (transformation != null) {
 			transformation.dispose();
@@ -220,24 +233,38 @@ public class StereotypedElementEventDrivenTransformation extends AdapterImpl{
 		transformation = null;
 		engine.dispose();
 	}
-	
-	private ConflictResolver createConflictResolver() throws InconsistentEventSemanticsException, ViatraQueryException{
-		FixedPriorityConflictResolver  fixedPriorityResolver = new FixedPriorityConflictResolver();
-        fixedPriorityResolver.setPriority(getAttributeCreationRule().getRuleSpecification(), 1);
-        fixedPriorityResolver.setPriority(getAttributeRedefinitionRule().getRuleSpecification(), 2);
-        fixedPriorityResolver.setPriority(getStereotypeRemovalRule().getRuleSpecification(), 3);
-        fixedPriorityResolver.setPriority(getBlockRestructuringRule().getRuleSpecification(), 4);
-        return fixedPriorityResolver;
+
+	/*
+	 * Creates a Conflict resolver that enables us to define a priority order among
+	 * transformation rules. this is needed as some rules depend on the results of
+	 * others
+	 */
+	private ConflictResolver createConflictResolver() throws InconsistentEventSemanticsException, ViatraQueryException {
+		FixedPriorityConflictResolver fixedPriorityResolver = new FixedPriorityConflictResolver();
+		fixedPriorityResolver.setPriority(getAttributeCreationRule().getRuleSpecification(), 1);
+		fixedPriorityResolver.setPriority(getAttributeRedefinitionRule().getRuleSpecification(), 2);
+		fixedPriorityResolver.setPriority(getStereotypeRemovalRule().getRuleSpecification(), 3);
+		fixedPriorityResolver.setPriority(getBlockRestructuringRule().getRuleSpecification(), 4);
+		return fixedPriorityResolver;
+	}
+
+	/*
+	 * Creates the custom scheduler that halts the execution of transformation rules
+	 * until the MD model reaches a stable state. This scheduler utilizes the
+	 * TransactionCommitListener API of MD.
+	 */
+	public static StereotypedTransformationSchedulerFactory getQueryEngineSchedulerFactory(
+			final ViatraQueryEngine engine) {
+		QueryEngineUpdateCompleteProvider provider;
+		provider = new QueryEngineUpdateCompleteProvider(engine);
+		return new StereotypedTransformationSchedulerFactory(provider);
 	}
 	
+	/*
+	 * Allows the UI components to query transformations related to the given Profile. Transformations are attached to profiles via the EMF Adapter mechanic.
+	 */
 	public static Optional<Adapter> findAdapter(Profile profile) {
 		Optional<Adapter> foundAdapter = profile.eAdapters().stream().filter(ad -> ad instanceof StereotypedElementEventDrivenTransformation).findAny();
 		return foundAdapter;
 	}
-	
-	public static StereotypedTransformationSchedulerFactory getQueryEngineSchedulerFactory(final ViatraQueryEngine engine) {
-        QueryEngineUpdateCompleteProvider provider;
-        provider = new QueryEngineUpdateCompleteProvider(engine);
-        return new StereotypedTransformationSchedulerFactory(provider);
-    }
 }
